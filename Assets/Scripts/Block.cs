9002b4b75cd0b60bd567d088f32c7f5628d4ea42
adel <upgamestudio@gmail.com>
2019-02-28 09:13:08 +0300

    Добавил условие победы и счетчик сломанных блоков

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,9 +6,23 @@ public class Block : MonoBehaviour
 {
 
     [SerializeField] AudioClip breakSound;
+
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
+        level.BlockDestroyed();
     }
 }
