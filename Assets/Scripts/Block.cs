7f854fa6c72a8dfc6f29b154dcdbda88e8dd33f3
adel <upgamestudio@gmail.com>
2019-02-28 17:34:38 +0300

    Добавил очки для игрока и вывод его

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -8,10 +8,11 @@ public class Block : MonoBehaviour
     [SerializeField] AudioClip breakSound;
 
     Level level;
-
+    GameStatus gameStatus;
     private void Start()
     {
         level = FindObjectOfType<Level>();
+        gameStatus = FindObjectOfType<GameStatus>();
         level.CountBreakableBlocks();
     }
     private void OnCollisionEnter2D(Collision2D collision)
@@ -21,6 +22,7 @@ public class Block : MonoBehaviour
 
     private void DestroyBlock()
     {
+        gameStatus.AddToScore();
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
         level.BlockDestroyed();
---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class GameStatus : MonoBehaviour
+{
+    [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroy = 83;
+
+    [SerializeField] int currentScore = 0;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroy;
+        scoreText.text = currentScore.ToString();
+    }
+}
